86b4e55db163dedfc097d0601eecb71363293827
SRIHARSHINI S B <sriharshinisb@gmail.com>
2026-06-03 20:53:19 +0530

    Add files via upload

---
+++ SQL/Inactive Users.sql
@@ -0,0 +1,8 @@
+3.Inactive Users
+SELECT *
+FROM Users
+WHERE user_id NOT IN (
+        SELECT user_id
+        FROM Registrations
+        WHERE registration_date >= CURDATE() - INTERVAL 90 DAY
+    );
